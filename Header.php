07cbd52c0bcd0b16765430e1a482a3ba02cf3896
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2024-06-24 14:17:02 +0200

    maj

---
+++ Header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="nav">
+        <nav>
+        <img class="logo" src="./img/logoannecy.png" alt="logoannecy" />
+       
+        <a>Catalogue</a>
+        <a>Notre histoire</a>
+        <a>Contact</a></div>
+      </header>
+</body>
+</html>
+
---
+++ Index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Project_php</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+  <?php include 'Header.php';?>
+ <div class="container">
+      <h1 class="titre1">Annecy : La Perle des Alpes</h1>
+      <p>
+        Annecy, située dans la région Auvergne-Rhône-Alpes en France, est une
+        ville pittoresque souvent surnommée la "Venise des Alpes" en raison de
+        ses canaux charmants et de son magnifique lac d'un bleu cristallin.
+        Entourée de montagnes majestueuses, Annecy offre un cadre naturel
+        exceptionnel qui attire les amoureux de la nature et les amateurs de
+        sports en plein air.
+      </p>
+
+      <h2>Activités à Annecy :</h2>
+      <ul>
+        <li>
+          <strong>Promenades et Randonnées :</strong>
+          <p>
+            Explorez les sentiers autour du lac d'Annecy et dans les montagnes
+            environnantes. Les randonnées vers le Mont Veyrier ou le Semnoz
+            offrent des vues panoramiques à couper le souffle.
+          </p>
+        </li>
+        <li>
+          <strong>Sports Nautiques :</strong>
+          <p>
+            Profitez du lac pour faire du paddle, du kayak, de la voile ou du
+            ski nautique. Les eaux claires du lac sont parfaites pour la
+            baignade en été.
+          </p>
+        </li>
+        <li>
+          <strong>Vieille Ville :</strong>
+          <p>
+            Flânez dans les rues pavées de la vieille ville d'Annecy avec ses
+            maisons colorées, ses boutiques artisanales et ses marchés locaux.
+            Ne manquez pas le Palais de l'Isle et le Château d'Annecy.
+          </p>
+        </li>
+        <li>
+          <strong>Cyclisme :</strong>
+          <p>
+            Parcourez la piste cyclable qui entoure le lac. C’est une activité
+            idéale pour toute la famille et offre des vues spectaculaires sur le
+            lac et les montagnes.
+          </p>
+        </li>
+        <li>
+          <strong>Festivals et Événements :</strong>
+          <p>
+            Assistez à des événements populaires comme le Festival International
+            du Film d'Animation d'Annecy en juin ou la Fête du Lac en août, qui
+            propose un grand spectacle pyrotechnique.
+          </p>
+        </li>
+      </ul>
+    </div>
+    <footer class="foot">
+      <p>Résaux</p>
+      <p>FAQ</p>
+    </footer>
+  </body>
+</html>
